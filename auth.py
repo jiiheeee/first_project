1fc372bddc9beb6a38e3def681758fa4648f01f7
H J <qkrwlgml33@gmail.com>
2023-09-14 23:40:57 +0900

    delete auth.py

--- auth.py
+++
@@ -1,9 +0,0 @@
-import boto3
-import os
-def auth(service):
-    service = boto3.client(
-        service_name = service, 
-        region_name='ap-northeast-2', 
-        aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    )
